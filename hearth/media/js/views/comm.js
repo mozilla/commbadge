50de51fa63d54f6ff0b3fdadfdcd50659e503072
Ashish Dubey <ashish.dubey91@gmail.com>
2013-07-26 13:07:26 -0700

    add Read/Unread/Recent filters (bug 897269)

--- hearth/media/js/views/comm.js
+++ hearth/media/js/views/comm.js
@@ -1,20 +1,21 @@
 define('views/comm',
-    ['l10n', 'z', 'urls', 'requests', 'nunjucks', 'notification'],
-    function(l10n, z, urls, requests, nunjucks, notification) {
+    ['l10n', 'z', 'urls', 'requests', 'nunjucks', 'notification', 'storage'],
+    function(l10n, z, urls, requests, nunjucks, notification, storage) {
     'use strict';
 
     var gettext = l10n.gettext;
     var ngettext = l10n.ngettext;
 
     var postNote = function($text_elem) {
-        var thread_id = $text_elem.data('thread-id');
+        var $threadItem = $text_elem.closest('.thread-item');
+        var thread_id = $threadItem.data('thread-id');
         var data = {
             note_type: 0,
-            body: $text_elem.val(),
+            body: $text_elem.val()
         };
 
         requests.post(urls.api.url('notes', [thread_id]), data).done(function(data) {
-            var $threadElem = $text_elem.closest('.thread-header');
+            var $threadElem = $threadItem.find('.thread-header');
 
             // Add a new note element.
             var noteMarkup = nunjucks.env.getTemplate('comm/note_detail.html').render({note: data});
@@ -60,9 +61,38 @@ define('views/comm',
 
     }).on('click', 'button.post', function(e) {
         postNote($(this).siblings('.reply-text'));
+
+    }).on('click', '.notes-filter', function(e) {
+        var $this = $(this);
+        var $threadItem = $this.closest('.thread-item');
+        var thread_id = $threadItem.data('thread-id');
+        var filterMode = $this.data('filter-mode');
+        var params = {ordering: '-created', limit: 5};
+
+        $('.notes-filter').removeClass('selected');
+        $this.addClass('selected');
+        $threadItem.data('notes-filter', filterMode);
+
+        switch (filterMode) {
+            case 'read':
+                params.show_read = '1';
+                break;
+            case 'unread':
+                params.show_read = '0';
+                break;
+        }
+
+        requests.get(urls.api.url('notes', [thread_id], params)).done(function(data) {
+            var markup = '';
+            data.objects.forEach(function(object) {
+                markup += nunjucks.env.getTemplate('comm/note_detail.html').render({note: object});
+            });
+            $threadItem.find('.notes-container').html(markup);
+        });
+
     });
 
     return function(builder) {
-        builder.start('comm/main.html');
+        builder.start('comm/main.html', {notes_filter: 'recent'});
     };
 });
