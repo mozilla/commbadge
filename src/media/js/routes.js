dda3fd82899987f08e2905d635b9a2a2a0d955e2
Ashish Dubey <ashish.dubey91@gmail.com>
2013-08-11 01:31:33 -0700

    use commonplace and cleanup

---
+++ src/media/js/routes.js
@@ -0,0 +1,26 @@
+(function() {
+
+// Please leave quotes around keys! They're needed for Space Heater.
+var routes = window.routes = [
+    {'pattern': '^/tests$', 'view_name': 'tests'},
+    {'pattern': '^/debug$', 'view_name': 'debug'},
+
+    {'pattern': '^/comm$', 'view_name': 'comm'},
+    {'pattern': '^/comm/$', 'view_name': 'comm'},
+    {'pattern': '^/comm/thread/([^/<>"\']+)$', 'view_name': 'show_thread'}
+];
+
+define(
+    'routes',
+    routes.map(function(i) {return 'views/' + i.view_name;}),
+    function() {
+        for (var i = 0; i < routes.length; i++) {
+            var route = routes[i];
+            var view = require('views/' + route.view_name);
+            route.view = view;
+        }
+        return routes;
+    }
+);
+
+})();
